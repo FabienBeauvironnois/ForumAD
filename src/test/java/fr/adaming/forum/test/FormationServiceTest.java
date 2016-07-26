ce4e231fd2393ed3d7898546ce7ff622d8e6a483
mroblin <mroblin@bitbucket.com>
2016-07-26 15:17:45 +0200

    Contrainte d'unicité Formation + tests

--- src/test/java/fr/adaming/forum/test/FormationServiceTest.java
+++ src/test/java/fr/adaming/forum/test/FormationServiceTest.java
@@ -60,7 +60,7 @@ public class FormationServiceTest {
 	@SuppressWarnings("deprecation")
 	@Test
 	public void testDeleteFormation() {
-		Formation formation = new Formation("JavaEE", "Toulouse", new Date(2016, 04, 18), new Date(2016, 07, 8) , false);
+		Formation formation = new Formation("JavaEE", "Paris", new Date(2016, 04, 18), new Date(2016, 07, 8) , false);
 		Long id = service.addFormation(formation).getIdFormation();
 		
 		formation = service.deleteFormation(id);
